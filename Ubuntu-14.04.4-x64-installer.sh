556cad8683d3f36163860025f1617ee8ddea195f
Dongdong Tian <seisman.info@gmail.com>
2016-03-30 00:51:11 +0800

    symbolic link does not work for github

--- Ubuntu-14.04.4-x64-installer.sh
+++ Ubuntu-14.04.4-x64-installer.sh
@@ -1,1 +1,7 @@
-Ubuntu-15.10-x64-installer.sh
+#!/bin/bash
+
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+sudo apt-get update
+sudo apt-get -y install gcc g++ cmake make libc6 ghostscript \
+                libglib2.0-dev libnetcdf-dev libgdal-dev python-gdal \
+                liblapack3 libpcre3-dev libfftw3-dev
