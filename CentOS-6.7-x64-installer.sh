5c7bc1df7a0b0679e4e6ffd4b5baf13b8a734c01
Dongdong Tian <seisman.info@gmail.com>
2016-03-30 23:38:04 +0800

    CentOS 6.7默认没有安装wget

--- CentOS-6.7-x64-installer.sh
+++ CentOS-6.7-x64-installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo yum install -y epel-release
 sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
-        glib2-devel gdal-devel gdal-python lapack-devel pcre-devel fftw-devel
+                    glib2-devel gdal-devel gdal-python lapack-devel \
+                    pcre-devel fftw-devel wget
