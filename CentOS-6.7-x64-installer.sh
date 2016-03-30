83c01c036daff183fd9018a281085476a5e046ce
Dongdong Tian <seisman.info@gmail.com>
2016-03-30 23:36:52 +0800

    netcdf等包在epel中，因而需要先安装epel-release，而yum安装时顺序是乱的，所以将epel-release的安装单独用一个命令

--- CentOS-6.7-x64-installer.sh
+++ CentOS-6.7-x64-installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo yum install -y epel-release gcc gcc-c++ cmake make glibc \
-                    ghostscript netcdf-devel glib2-devel gdal-devel gdal-python \
-                    lapack-devel pcre-devel fftw-devel
+sudo yum install -y epel-release
+sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
+        glib2-devel gdal-devel gdal-python lapack-devel pcre-devel fftw-devel
--- CentOS-7.2-x64-installer.sh
+++ CentOS-7.2-x64-installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo yum install -y epel-release gcc gcc-c++ cmake make glibc \
-                    ghostscript netcdf-devel glib2-devel gdal-devel gdal-python \
-                    lapack-devel lapack64-devel pcre-devel fftw-devel
+sudo yum install -y epel-release
+sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
+                    glib2-devel gdal-devel gdal-python lapack-devel lapack64-devel \
+                    pcre-devel fftw-devel
