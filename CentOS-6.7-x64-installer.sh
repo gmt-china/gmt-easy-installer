ac44cbc15f9da229aa5fc03ba58e8a82d4c437b8
Dongdong Tian <seisman.info@gmail.com>
2016-04-08 18:37:17 +0800

    Fix CentOS 6.7
    1. GLib的版本号为2.28，小于GMT所需要的2.32，因而给出警告
    2. 自带的netCDF不依赖于zlib，因而需要单独安装

--- CentOS-6.7-x64-installer.sh
+++ CentOS-6.7-x64-installer.sh
@@ -2,4 +2,8 @@
 sudo yum install -y epel-release
 sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
                     glib2-devel gdal-devel gdal-python lapack-devel \
-                    pcre-devel fftw-devel wget
+                    pcre-devel fftw-devel wget zlib-devel
+
+echo "######################################################################"
+echo "# WARNING: Modify GMT_USE_THREADS to FALSE in gmt-5.2.1-installer.sh #"
+echo "######################################################################"
