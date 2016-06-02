db9459e22daf05f71f877b5b5b42b56d18fc132a
Dongdong Tian <seisman.info@gmail.com>
2016-06-02 20:39:15 +0800

    自动修改GMT安装脚本

--- GMT-5.2.1/CentOS-6.7-installer.sh
+++ GMT-5.2.1/CentOS-6.7-installer.sh
@@ -4,6 +4,10 @@ sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
                     glib2-devel gdal-devel gdal-python lapack-devel \
                     pcre-devel fftw-devel wget zlib-devel
 
-echo "######################################################################"
-echo "# WARNING: Modify GMT_USE_THREADS to FALSE in gmt-5.2.1-installer.sh #"
-echo "######################################################################"
+if [ -e GMT-*-installer.sh ]; then
+    sed -i 's/(GMT_USE_THREADS TRUE)/(GMT_USE_THREADS FALSE)/' GMT-*-installer.sh
+else
+    echo "######################################################################"
+    echo "# WARNING: Modify GMT_USE_THREADS to FALSE in GMT-*-installer.sh     #"
+    echo "######################################################################"
+fi
