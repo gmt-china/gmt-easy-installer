82e5ffbd37c09c9dc33eca8c40563bdd9a63c6e8
Dongdong Tian <seisman.info@gmail.com>
2016-06-04 16:42:52 +0800

    support Ubuntu 16.04

--- GMT-5.2.1/GMT-5.2.1-installer.sh
+++ GMT-5.2.1/GMT-5.2.1-installer.sh
@@ -34,6 +34,12 @@ set (COPY_DCW TRUE)
 set (GMT_USE_THREADS TRUE)
 EOF
 
+# patch for GMT5.2.1 on Ubuntu 16.04
+platform=`lsb_release -ds`
+if [ "$platform" = "Ubuntu 16.04 LTS" ]; then
+    sed -i '752s/^.*$/(GMT->common.d.active[GMT_IN]) ? sprintf (e_value, GMT->current.setting.format_float_out, GMT->common.d.nan_proxy[GMT_IN]) : sprintf (e_value, "NaN");/' src/xyz2grd.c
+fi
+
 mkdir build
 cd build
 cmake ..
