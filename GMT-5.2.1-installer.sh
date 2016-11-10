807671ce592cd1ea6a6b19c76032fdda9d39ba16
Dongdong Tian <seisman.info@gmail.com>
2016-11-10 18:51:38 +0800

    freeze GMT-5.2.1

--- GMT-5.2.1-installer.sh
+++ GMT-5.2.1-installer.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 GMT_VERSION=5.2.1
-GSHHG_VERSION=2.3.6
-DCW_VERSION=1.1.2
+GSHHG_VERSION=2.3.5
+DCW_VERSION=1.1.1
 GMT_INSTALL=/opt/GMT-${GMT_VERSION}
+#GMT_MIRROR=ftp://ftp.soest.hawaii.edu/gmt
+GMT_MIRROR=http://mirrors.ustc.edu.cn/gmt/legacy/
 
 echo "Downloading files..."
-curl -C - -O ftp://ftp.soest.hawaii.edu/gmt/gmt-${GMT_VERSION}-src.tar.gz
-curl -C - -O ftp://ftp.soest.hawaii.edu/gmt/gshhg-gmt-${GSHHG_VERSION}.tar.gz
-curl -C - -O ftp://ftp.soest.hawaii.edu/gmt/dcw-gmt-${DCW_VERSION}.tar.gz
+curl -C - -O ${GMT_MIRROR}/gmt-${GMT_VERSION}-src.tar.gz
+curl -C - -O ${GMT_MIRROR}/gshhg-gmt-${GSHHG_VERSION}.tar.gz
+curl -C - -O ${GMT_MIRROR}/dcw-gmt-${DCW_VERSION}.tar.gz
 
 # Verify the integrity of files
 if ! md5sum --status -c md5sums.md5; then
@@ -36,7 +38,7 @@ EOF
 
 # patch for GMT5.2.1 on Ubuntu 16.04
 platform=`lsb_release -ds`
-if [ "$platform" = "Ubuntu 16.04 LTS" ]; then
+if [[ "$platform" =~ "Ubuntu 16.04" ]]; then
     sed -i '752s/^.*$/(GMT->common.d.active[GMT_IN]) ? sprintf (e_value, GMT->current.setting.format_float_out, GMT->common.d.nan_proxy[GMT_IN]) : sprintf (e_value, "NaN");/' src/xyz2grd.c
 fi
 
