efbb48b363a71b0e7ae54435a40af873bca7457f
Dongdong Tian <seisman.info@gmail.com>
2016-11-10 20:31:56 +0800

    update to GMT 5.3.1

--- GMT-5.3.1-installer.sh
+++ GMT-5.3.1-installer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-GMT_VERSION=5.2.1
-GSHHG_VERSION=2.3.5
-DCW_VERSION=1.1.1
+GMT_VERSION=5.3.1
+GSHHG_VERSION=2.3.6
+DCW_VERSION=1.1.2
 GMT_INSTALL=/opt/GMT-${GMT_VERSION}
 #GMT_MIRROR=ftp://ftp.soest.hawaii.edu/gmt
-GMT_MIRROR=http://mirrors.ustc.edu.cn/gmt/legacy/
+GMT_MIRROR=http://mirrors.ustc.edu.cn/gmt/
 
 echo "Downloading files..."
 curl -C - -O ${GMT_MIRROR}/gmt-${GMT_VERSION}-src.tar.gz
@@ -35,12 +35,6 @@ set (COPY_GSHHG TRUE)
 set (COPY_DCW TRUE)
 set (GMT_USE_THREADS TRUE)
 EOF
-
-# patch for GMT5.2.1 on Ubuntu 16.04
-platform=`lsb_release -ds`
-if [[ "$platform" =~ "Ubuntu 16.04" ]]; then
-    sed -i '752s/^.*$/(GMT->common.d.active[GMT_IN]) ? sprintf (e_value, GMT->current.setting.format_float_out, GMT->common.d.nan_proxy[GMT_IN]) : sprintf (e_value, "NaN");/' src/xyz2grd.c
-fi
 
 mkdir build
 cd build
