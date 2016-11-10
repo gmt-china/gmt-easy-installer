75ea0e0fa6bdad8e53abc6be99a494dcbed90861
Dongdong Tian <seisman.info@gmail.com>
2016-11-10 20:08:52 +0800

    fix travis

--- .test-in-docker.sh
+++ .test-in-docker.sh
@@ -48,7 +48,7 @@ fi
 
 sudo docker run --rm=true --privileged=true -v `pwd`:${BASE_dir}:rw ${DOCKER} /bin/bash -c "
         ${more_cmd}
-        cd ${BASE_dir}/GMT-${GMT_version}/
+        cd ${BASE_dir}/
         bash ${OS_installer}
         bash ${GMT_installer}
         /opt/GMT-${GMT_version}/bin/gmt --version
