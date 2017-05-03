d50e93cd94c01b804dff2419f9b15cad1d66d631
Dongdong Tian <seisman.info@gmail.com>
2017-05-03 20:39:08 +0800

    fix

--- centos-installer.sh
+++ centos-installer.sh
@@ -6,11 +6,11 @@ sudo yum install -y gcc gcc-c++ cmake make glibc ghostscript netcdf-devel \
 
 release=`lsb_release -r | cut -f2 | cut -c1`
 if [ $release = '6' ]; then
-    if [ -e GMT-*-installer.sh ]; then
-        sed -i 's/(GMT_USE_THREADS TRUE)/(GMT_USE_THREADS FALSE)/' GMT-*-installer.sh
+    if [ -e GMT-installer.sh ]; then
+        sed -i 's/(GMT_USE_THREADS TRUE)/(GMT_USE_THREADS FALSE)/' GMT-installer.sh
     else
         echo "######################################################################"
-        echo "# WARNING: Modify GMT_USE_THREADS to FALSE in GMT-*-installer.sh     #"
+        echo "# WARNING: Modify GMT_USE_THREADS to FALSE in GMT-installer.sh     #"
         echo "######################################################################"
     fi
 elif [ $release = '7' ]; then
